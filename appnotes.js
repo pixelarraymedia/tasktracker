64f3fb7bb2abb8dbdd9c62f342c63fa494d4258f
Darion <pixelarraymedia@gmail.com>
2022-06-28 15:11:00 -0700

    added annoted app.js file

--- appnotes.js
+++ appnotes.js
@@ -9,14 +9,22 @@ const app = Vue.createApp({
         } 
     },
 
-
+    // defining method for onclick function
 methods: {
+
+   // using Fetch API and Async Await we will call random user API 
 
   async  getUser() {
 
+            // console.log(this.firstName) > should return defined firstName in dom
 
         const res = await fetch('https://randomuser.me/api')
+
+                // Return result array as JSON in results variable
+                
         const { results } = await res.json()
+
+       // console.log(results)
 
         this.firstName = results[0].name.first
         this.lastName = results[0].name.last
